1d035ec5e8afaa286594ad3e77858cdd0119c5c0
Sean Kozen <sgkozen@gmail.com>
2023-03-31 11:12:04 +0900

    List created

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,8 +1,8 @@
-function Todos() {
+
+const Todos: React.FC<{items: string[]}> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn Typescript</li>
+      {props.items.map(item => <li key={item}>{item}</li>)}
     </ul>
   );
 }
