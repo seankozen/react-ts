f1234c9243c33e8bf3e91429f4226a13b6a67668
Sean Kozen <sgkozen@gmail.com>
2023-03-31 16:26:24 +0900

    NewToDo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import Todos from './components/Todos';
-import Todo from './models/todo';
 
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn Typescript'),
-  ];
+  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
+
+  const addToDoHandler = (todoText: string) => {
+
+  };
+
+
   return (
     <div>
-      <Todos items={todos}/>
+      <NewTodo onAddTodo={addToDoHandler} />
+      <Todos items={todos} />
     </div>
   );
 }
